dac9ed23c8622efe63220624f4debfc4d134d4b8
Emmanuel <emmanuel.oladipo@oi-devs.com>
2022-05-08 01:14:03 +0300

    Possible fix for clipping issue

--- lib/src/ui/views/devotional_page/devotional_page.dart
+++ lib/src/ui/views/devotional_page/devotional_page.dart
@@ -16,76 +16,78 @@ class DevotionalPage extends StatefulWidget {
 }
 
 class _DevotionalPageState extends State<DevotionalPage> {
-  var _title='';
-  var _memoryVerse='';
-  var _memoryVersePassage='';
-  var _mainWriteUp='';
-  var _prayerBurden='';
-  var _thoughtOfTheDay='';
-
+  var _title = '';
+  var _memoryVerse = '';
+  var _memoryVersePassage = '';
+  var _mainWriteUp = '';
+  var _prayerBurden = '';
+  var _thoughtOfTheDay = '';
 
   @override
   void initState() {
     getTodayTitleAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
     getVersePassageAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
     getVerseAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
-    getTodayMainWriteUpAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
+    getTodayMainWriteUpAsString(
+        DateFormat('dd.MM.yyyy').format(DateTime.now()));
     getTodayPrayerAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
-    getTodayThoughtOfTheDayAsString(DateFormat('dd.MM.yyyy').format(DateTime.now()));
-
-
+    getTodayThoughtOfTheDayAsString(
+        DateFormat('dd.MM.yyyy').format(DateTime.now()));
   }
 
   @override
   Widget build(BuildContext context) {
     final List<Widget> devotionalCards = [
-      Expanded(
-        child: Card(
-          color: CantonMethods.alternateCanvasColorType2(context),
-          shape: CantonSmoothBorder.defaultBorder(),
-          child: Column(
-            children: [
-              ListTile(
-                title: Text('Topic for today:',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4
-                        ?.copyWith(fontWeight: FontWeight.bold)),
-              ),
-              const SizedBox(height: 5),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(_title,
+      Card(
+        color: CantonMethods.alternateCanvasColorType2(context),
+        shape: CantonSmoothBorder.defaultBorder(),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Wrap(
+              children: [
+                Column(children: [
+                  ListTile(
+                    title: Text('Topic for today:',
                         style: Theme.of(context)
                             .textTheme
-                            .headline5
-                            ?.copyWith(fontWeight: FontWeight.normal))),
-              ),
-              const SizedBox(height: 15),
-              ListTile(
-                title: Text('Memory Verse:',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4
-                        ?.copyWith(fontWeight: FontWeight.bold)),
-              ),
-              const SizedBox(height: 5),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(_memoryVerse + " " + _memoryVersePassage,
+                            .headline4
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(_title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                ?.copyWith(fontWeight: FontWeight.normal))),
+                  ),
+                  const SizedBox(height: 15),
+                  ListTile(
+                    title: Text('Memory Verse:',
                         style: Theme.of(context)
                             .textTheme
-                            .headline5
-                            ?.copyWith(fontWeight: FontWeight.normal))),
-              ),
-            ],
-          ),
-        ),
+                            .headline4
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(_memoryVerse + " " + _memoryVersePassage,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                ?.copyWith(fontWeight: FontWeight.normal))),
+                  ),
+                ])
+              ],
+            )),
       ),
+
       // Expanded(
       //   child: Card(
       //   color: CantonMethods.alternateCanvasColorType2(context),
@@ -104,79 +106,100 @@ class _DevotionalPageState extends State<DevotionalPage> {
       Card(
         color: CantonMethods.alternateCanvasColorType2(context),
         shape: CantonSmoothBorder.defaultBorder(),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text('Word',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline4
-                      ?.copyWith(fontWeight: FontWeight.bold)),
-              trailing: Text(
-                'More',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6
-                    ?.copyWith(fontWeight: FontWeight.normal),
-              ),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(_mainWriteUp,
-                  maxLines: 12,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      ?.copyWith(fontWeight: FontWeight.normal)),
-            )
-          ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Wrap(
+            children: [
+              Column(
+                children: [
+                  ListTile(
+                    title: Text('Word',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline4
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                    trailing: Text(
+                      'More',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          ?.copyWith(fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(_mainWriteUp,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 10,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(fontWeight: FontWeight.normal)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
       Card(
         color: CantonMethods.alternateCanvasColorType2(context),
         shape: CantonSmoothBorder.defaultBorder(),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text('Prayer',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      ?.copyWith(fontWeight: FontWeight.bold)),
-            ),
-            const SizedBox(height: 5),
-            Text(_prayerBurden,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline3
-                    ?.copyWith(fontWeight: FontWeight.normal))
-          ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Wrap(
+            children: [
+              Column(
+                children: [
+                  ListTile(
+                    title: Text('Prayer',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(_prayerBurden,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3
+                          ?.copyWith(fontWeight: FontWeight.normal))
+                ],
+              )
+            ],
+          ),
         ),
       ),
       Card(
         color: CantonMethods.alternateCanvasColorType2(context),
         shape: CantonSmoothBorder.defaultBorder(),
-        child: Column(
-          children: [
-            ListTile(
-              title: Text('Thought for The Day',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5
-                      ?.copyWith(fontWeight: FontWeight.bold)),
-            ),
-            const SizedBox(height: 5),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(_thoughtOfTheDay,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3
-                      ?.copyWith(fontWeight: FontWeight.normal)),
-            )
-          ],
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Wrap(
+            children: [
+              Column(
+                children: [
+                  ListTile(
+                    title: Text('Thought for The Day',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(_thoughtOfTheDay,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline3
+                            ?.copyWith(fontWeight: FontWeight.normal)),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ];
@@ -189,40 +212,42 @@ class _DevotionalPageState extends State<DevotionalPage> {
               child: VerticalCardPager(
                   images: devotionalCards,
                   titles: const ['', '', '', ''],
-                  onPageChanged: (page) {},
+                  onPageChanged: (page) {
+                  },
                   onSelectedItem: (index) {
                     switch (index) {
                       case 0:
                         Navigator.of(context).push(PageTransition(
-                            child: FullTopicMemoryVerseVersePage(title: _title, memoryVerse: _memoryVerse, memoryVersePassage: _memoryVersePassage),
+                            child: FullTopicMemoryVerseVersePage(
+                                title: _title,
+                                memoryVerse: _memoryVerse,
+                                memoryVersePassage: _memoryVersePassage),
                             type: PageTransitionType.scale,
                             alignment: Alignment.center,
-                            duration: const Duration(milliseconds: 600)
-                        ));
+                            duration: const Duration(milliseconds: 600)));
                         break;
                       case 1:
                         Navigator.of(context).push(PageTransition(
-                            child: FullWordContentPage(mainWriteUp: _mainWriteUp),
+                            child:
+                                FullWordContentPage(mainWriteUp: _mainWriteUp),
                             type: PageTransitionType.scale,
                             alignment: Alignment.center,
-                        duration: const Duration(milliseconds: 600)
-                        ));
+                            duration: const Duration(milliseconds: 600)));
                         break;
                       case 2:
                         Navigator.of(context).push(PageTransition(
                             child: FullPrayerPage(prayer: _prayerBurden),
                             type: PageTransitionType.scale,
                             alignment: Alignment.center,
-                            duration: const Duration(milliseconds: 600)
-                        ));
+                            duration: const Duration(milliseconds: 600)));
                         break;
                       case 3:
                         Navigator.of(context).push(PageTransition(
-                            child: FullThoughtOfTheDayPage(thoughtOfTheDay: _thoughtOfTheDay),
+                            child: FullThoughtOfTheDayPage(
+                                thoughtOfTheDay: _thoughtOfTheDay),
                             type: PageTransitionType.scale,
                             alignment: Alignment.center,
-                            duration: const Duration(milliseconds: 600)
-                        ));
+                            duration: const Duration(milliseconds: 600)));
                         break;
                     }
                   },
@@ -237,13 +262,14 @@ class _DevotionalPageState extends State<DevotionalPage> {
   }
 
   getTodayTitleAsString(String dt) async {
-    var title =   await DevotionalItemsRetrieveClass.getTodayTitle(dt);
+    var title = await DevotionalItemsRetrieveClass.getTodayTitle(dt);
     setState(() {
       _title = title;
     });
   }
+
   getVerseAsString(String dt) async {
-    var verse =   await DevotionalItemsRetrieveClass.getTodayVerse(dt);
+    var verse = await DevotionalItemsRetrieveClass.getTodayVerse(dt);
     //print(verse);
     setState(() {
       _memoryVerse = verse;
@@ -251,21 +277,23 @@ class _DevotionalPageState extends State<DevotionalPage> {
   }
 
   getVersePassageAsString(String dt) async {
-    var versePassage =   await DevotionalItemsRetrieveClass.getTodayVersePassage(dt);
+    var versePassage =
+        await DevotionalItemsRetrieveClass.getTodayVersePassage(dt);
     setState(() {
       _memoryVersePassage = versePassage;
     });
   }
 
   getTodayMainWriteUpAsString(String dt) async {
-    var mainWriteUp =   await DevotionalItemsRetrieveClass.getTodayMainWriteUp(dt);
+    var mainWriteUp =
+        await DevotionalItemsRetrieveClass.getTodayMainWriteUp(dt);
     setState(() {
       _mainWriteUp = mainWriteUp;
     });
   }
 
   getTodayPrayerAsString(String dt) async {
-    var prayerBurden =   await DevotionalItemsRetrieveClass.getTodayPrayer(dt);
+    var prayerBurden = await DevotionalItemsRetrieveClass.getTodayPrayer(dt);
     setState(() {
       _prayerBurden = prayerBurden;
       print('mmmmmmmmmmmmmmmmmmmmmm');
@@ -274,7 +302,8 @@ class _DevotionalPageState extends State<DevotionalPage> {
   }
 
   getTodayThoughtOfTheDayAsString(String dt) async {
-    var thoughtOfTheDay =   await DevotionalItemsRetrieveClass.getTodayThoughtOfTheDay(dt);
+    var thoughtOfTheDay =
+        await DevotionalItemsRetrieveClass.getTodayThoughtOfTheDay(dt);
     setState(() {
       _thoughtOfTheDay = thoughtOfTheDay;
       print('mmmmmmmmmmmmmmmmmmmmmm');
