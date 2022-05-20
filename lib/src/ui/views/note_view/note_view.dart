4451f71717d8776abc7e38294026f17745fa2e98
Igbana <igbanaaondosoo13@gmail.com>
2022-05-20 18:36:53 -0400

    Unable to fix timeout, but fixed mic icon controll and append text in Note entrybox

--- lib/src/ui/views/note_view/note_view.dart
+++ lib/src/ui/views/note_view/note_view.dart
@@ -16,13 +16,14 @@ class _DevotionalNotePageState extends State<DevotionalNotePage> {
   bool _islistening = false;
   double confidence = 1.0;
   var noteWidget = TextEditingController();
-  String? jottings;
-  String? writeup;
+  String newWords ="";
+
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _speech = SpeechToText();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -36,14 +37,16 @@ class _DevotionalNotePageState extends State<DevotionalNotePage> {
                 child: Row(children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text('Note', style: Theme.of(context).textTheme.headline3),
+                    child: Text('Note',
+                        style: Theme.of(context).textTheme.headline3),
                   ),
                   Expanded(
                       child: Align(
                           alignment: Alignment.centerRight,
                           child: IconButton(
                               onPressed: _listen,
-                              icon: Icon(_islistening ? Icons.mic_off : Icons.mic))))
+                              icon: Icon(
+                                  _islistening ? Icons.mic_off : Icons.mic))))
                 ]),
               ),
               Align(
@@ -73,10 +76,6 @@ class _DevotionalNotePageState extends State<DevotionalNotePage> {
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
                       keyboardType: TextInputType.text,
-                      //initialValue: jottings,
-                      onChanged: (value){
-                        noteWidget.text = noteWidget.text + value;
-                      },
                       decoration: const InputDecoration(
                           alignLabelWithHint: true,
                           labelText: 'Title',
@@ -108,21 +107,18 @@ class _DevotionalNotePageState extends State<DevotionalNotePage> {
                 height: 5,
               ),
               GestureDetector(
-                  onTap: () {
-                  },
+                  onTap: () {},
                   child: Container(
                       width: MediaQuery.of(context).size.width - 40,
                       height: 50,
                       decoration: BoxDecoration(
                           color: Colors.black,
-                        borderRadius: BorderRadius.circular(15)
-                      ),
+                          borderRadius: BorderRadius.circular(15)),
                       child: const Align(
                           alignment: Alignment.center,
                           child: Text(
                             "Save",
-                            style: TextStyle(
-                                fontSize: 18),
+                            style: TextStyle(fontSize: 18),
                           ))))
             ],
           ),
@@ -130,18 +126,32 @@ class _DevotionalNotePageState extends State<DevotionalNotePage> {
       ),
     );
   }
+
   void _listen() async {
-    if (!_islistening){
+    if (!_islistening) {
       bool available = await _speech.initialize(
-          onStatus: (val) => print('onstatus: $val'),
-          onError: (val) => print('onError: $val')
-      );
+          onStatus: (val) => setState(() {
+                if (val == 'listening') {
+                  _islistening = true;
+                }
+                else if(val == 'done'){
+                  noteWidget.text = noteWidget.text == "" ? newWords + "." : noteWidget.text + "\n" + newWords + ".";
+                  //TODO: Use toasts to say "Tap microphone to speak again"
+                  //TODO: Capitalize first letter
+                }
+                else{
+                  _islistening = false;
+                }
+              }),
+          onError: (val) => print('onError: $val'));
       if (available) {
         setState(() => _islistening = true);
         _speech.listen(
-            onResult: (val) => setState(() => noteWidget.text = val.recognizedWords)
+          onResult: (val) => setState(
+              () => newWords = val.recognizedWords),
         );
-      };
+      }
+      ;
     } else {
       setState(() => _islistening = false);
       _speech.stop();
