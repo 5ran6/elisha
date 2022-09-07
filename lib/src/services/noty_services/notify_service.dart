d80f3a03d4d903742e2d171b9f63c2486b4cf802
Igbana <igbanaaondosoo13@gmail.com>
2022-09-07 19:10:36 +0100

    Alarm for iOS set

--- lib/src/services/noty_services/notify_service.dart
+++ lib/src/services/noty_services/notify_service.dart
@@ -3,15 +3,13 @@ import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
 class NotificationService {
-  static final NotificationService _notificationService =
-      NotificationService._internal();
+  static final NotificationService _notificationService = NotificationService._internal();
 
   factory NotificationService() {
     return _notificationService;
   }
 
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   NotificationService._internal();
 
@@ -19,23 +17,19 @@ class NotificationService {
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@drawable/app_icon');
 
-    const IOSInitializationSettings initializationSettingsIOS =
-        IOSInitializationSettings(
+    const IOSInitializationSettings initializationSettingsIOS = IOSInitializationSettings(
       requestAlertPermission: true,
       requestBadgePermission: true,
       requestSoundPermission: true,
     );
 
     const InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: initializationSettingsAndroid,
-            iOS: initializationSettingsIOS);
+        InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
     flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
-  Future<void> showNotification(
-      int id, String title, String body) async {
+  Future<void> showNotification(int id, String title, String body, String time) async {
     tz.initializeTimeZones();
     final detroit = tz.getLocation('America/Detroit');
     //2022-06-24 08:45:58.985497-0400
@@ -44,23 +38,19 @@ class NotificationService {
       id,
       title,
       body,
-      tz.TZDateTime.now(detroit).add(const Duration(seconds: 1)),
-      // (tz.TZDateTime.parse(detroit,
-      //                 "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00")
-      //             .difference(tz.TZDateTime.now(detroit)))
-      //         .isNegative
-      //     ? tz.TZDateTime.parse(detroit,
-      //             "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00")
-      //         .add(const Duration(days: 1))
-      //     : tz.TZDateTime.parse(detroit,
-      //     "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00"),
-      // //tz.TZDateTime.utc(DateTime.now().year, DateTime.now().month, DateTime.now().day, hour, minute).add(Duration(seconds: 2)),
+      (tz.TZDateTime.parse(detroit,
+                      "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00")
+                  .difference(tz.TZDateTime.now(detroit)))
+              .isNegative
+          ? tz.TZDateTime.parse(detroit,
+                  "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00")
+              .add(const Duration(days: 1))
+          : tz.TZDateTime.parse(detroit,
+              "${DateTime.now().year}${DateTime.now().month < 10 ? "0" + DateTime.now().month.toString() : DateTime.now().day}${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day} $time:00"),
+      //tz.TZDateTime.utc(DateTime.now().year, DateTime.now().month, DateTime.now().day, hour, minute).add(Duration(seconds: 2)),
       const NotificationDetails(
-        android: AndroidNotificationDetails(
-            'main_channel', 'Main Channel', 'Main channel notifications',
-            importance: Importance.max,
-            priority: Priority.max,
-            icon: '@drawable/app_icon'),
+        android: AndroidNotificationDetails('main_channel', 'Main Channel', 'Main channel notifications',
+            importance: Importance.max, priority: Priority.max, icon: '@drawable/app_icon'),
         iOS: IOSNotificationDetails(
           sound: 'default.wav',
           presentAlert: true,
@@ -68,8 +58,7 @@ class NotificationService {
           presentSound: true,
         ),
       ),
-      uiLocalNotificationDateInterpretation:
-          UILocalNotificationDateInterpretation.absoluteTime,
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
       androidAllowWhileIdle: true,
     );
   }
--- lib/src/ui/views/settings_view/settings_view.dart
+++ lib/src/ui/views/settings_view/settings_view.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+import 'dart:async';
+import 'dart:ui';
 
 import 'package:canton_design_system/canton_design_system.dart';
-import 'package:elisha/src/services/alarm_services.dart';
 import 'package:elisha/src/services/shared_pref_manager/shared_pref_manager.dart';
 import 'package:elisha/src/ui/views/settings_view/settings_header_view.dart';
 import 'package:flutter/foundation.dart';
@@ -12,7 +13,58 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:elisha/src/providers/theme_manager_provider.dart';
 
-import '../../../services/noty_services/notify_service.dart';
+import 'package:elisha/src/services/noty_services/notify_service.dart';
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      isForegroundMode: false,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+  service.startService();
+}
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('FLUTTER BACKGROUND FETCH');
+  return true;
+}
+
+void onStart(ServiceInstance service) async {
+  // Only available for flutter 3.0.0 and later
+  DartPluginRegistrant.ensureInitialized();
+
+  // For flutter prior to version 3.0.0
+  // We have to register the plugin manually
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+  await NotificationService().initNotification();
+  Timer.periodic(const Duration(seconds: 10), (timer) async {
+    String alarmTime = "";
+    String newTime = PrefManager.getTime() ?? 'Off';
+    if (newTime == 'Off') {
+      print('Time still null');
+    } else if (newTime != alarmTime) {
+      alarmTime = newTime;
+      print("Shared pref: $alarmTime");
+      NotificationService().showNotification(1, "title", "body", alarmTime);
+    }
+  });
+}
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
